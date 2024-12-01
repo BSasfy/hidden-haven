56208afb543b79d9d20c75c82b3ce5bffadac4a4
Barbara Sasfy <barbara.sasfy@gmail.com>
2024-12-01 14:03:00 +0000

    homepage and navbar changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/app/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { NavBar } from "./_components/nav-bar/NavBar";
+import { Footer } from "./_components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -16,8 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <NavBar />
-        {children}
+        <div className="h-[100vh]">
+          <NavBar />
+          {children}
+          <div className="align-bottom">
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
