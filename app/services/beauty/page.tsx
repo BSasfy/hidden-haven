d2bd5c3674b62014541ebc4de51858733cadf916
Barbara Sasfy <barbara.sasfy@gmail.com>
2024-12-02 12:53:31 +0000

    services pages

--- app/services/beauty/page.tsx
+++ app/services/beauty/page.tsx
@@ -1,3 +1,25 @@
-export default function ServicesPage() {
-  return <div className="text-3xl text-black">Beauty</div>;
+export default function BeautyPage() {
+  return (
+    <div className="text-[#FFFBF2]">
+      <div className="h-40 content-center bg-[url('/beauty.png')] bg-cover bg-center bg-no-repeat object-cover text-center md:h-72">
+        <div className="p-4 text-7xl font-light italic drop-shadow-2xl">
+          Beauty
+        </div>
+      </div>
+      <div className="px-20 py-12 font-extralight">
+        <div className="pb-9 text-4xl">Beauty</div>
+        <div className="pb-7 text-3xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor{" "}
+        </div>
+        <div className="justify-self-center">
+          <button className="rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl">
+            Book Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/services/talking-therapies/page.tsx
+++ app/services/talking-therapies/page.tsx
@@ -1,3 +1,25 @@
 export default function ServicesPage() {
-  return <div className="text-3xl text-black">Talking Therapies</div>;
+  return (
+    <div className="text-[#FFFBF2]">
+      <div className="h-40 content-center bg-[url('/talking-therapies.png')] bg-cover bg-center bg-no-repeat object-cover text-center md:h-72">
+        <div className="p-4 text-7xl font-light italic drop-shadow-2xl">
+          Talking Therapies
+        </div>
+      </div>
+      <div className="px-20 py-12 font-extralight">
+        <div className="pb-9 text-4xl">Talking Therapies</div>
+        <div className="pb-7 text-3xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor{" "}
+        </div>
+        <div className="justify-self-center">
+          <button className="rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl">
+            Book Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
