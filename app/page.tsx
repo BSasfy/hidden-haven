fc9122803067629f6d67e0214a88cfc91ca3f774
BSasfy <barbara.sasfy@gmail.com>
2024-12-03 12:00:37 +0000

    homepage book now button position

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 export default function HomePage() {
   return (
     <main>
-      <div className="relative h-[80vh] w-full content-center justify-items-center bg-[url('/homepage-hands.jpg')] bg-cover bg-center bg-no-repeat">
-        <div className="mb-[10%] w-4/5 text-center text-4xl font-medium italic text-white md:w-1/2 lg:text-5xl">
+      <div className="mb-7 flex h-[80vh] w-full flex-col content-center justify-end bg-[url('/homepage-hands.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="mx-auto mb-[250px] block w-4/5 justify-self-center text-center text-3xl font-medium italic text-white md:w-1/2 md:text-4xl lg:text-5xl">
           For mental, physical and spiritual wellbeing
         </div>
-        <div className="w-full justify-items-center">
-          <button className="absolute bottom-7 left-[36vw] rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl md:left-[43%]">
-            Book Now
-          </button>
-        </div>
+
+        <button className="mx-auto block rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl">
+          Book Now
+        </button>
       </div>
     </main>
   );
