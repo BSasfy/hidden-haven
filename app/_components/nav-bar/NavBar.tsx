9076ae5014f29c29ea11e2ccafae1ddf7bdfd03a
Barbara Sasfy <barbara.sasfy@gmail.com>
2024-12-03 00:20:43 +0000

    hamburger menu v1

--- app/_components/nav-bar/NavBar.tsx
+++ app/_components/nav-bar/NavBar.tsx
@@ -10,21 +10,50 @@ export function NavBar() {
   return (
     <nav className="bg-[#181818]">
       <div className="mx-auto flex max-w-screen-xl flex-wrap p-3">
-        {/* Hamburger menu button */}
-        <button
-          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 md:hidden"
-          onClick={() => {
-            setIsMobileMenuOpen(!isMobileMenuOpen);
-          }}
-        >
-          <span className="sr-only">Open main menu</span>
-        </button>
-        <div
-          data-mobilenavbar={isMobileMenuOpen}
-          className="absolute data-[mobilenavbar=false]:hidden"
-        >
-          <MobileNavBar />
+        <div>
+          {/* Hamburger menu button */}
+          {/* <button
+            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-300 focus:outline-none focus:ring-2 md:hidden"
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+            }}
+          >
+            <span className="sr-only">Open main menu</span>
+          </button> */}
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+            }}
+            className="flex flex-col items-center justify-center p-4 md:hidden"
+          >
+            <span
+              className={`block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+                isMobileMenuOpen
+                  ? "translate-y-1 rotate-45"
+                  : "-translate-y-0.5"
+              }`}
+            ></span>
+            <span
+              className={`my-0.5 block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+                isMobileMenuOpen ? "opacity-0" : "opacity-100"
+              }`}
+            ></span>
+            <span
+              className={`block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+                isMobileMenuOpen
+                  ? "-translate-y-1 -rotate-45"
+                  : "translate-y-0.5"
+              }`}
+            ></span>
+          </button>
+          <div
+            data-mobilenavbar={isMobileMenuOpen}
+            className="absolute z-10 h-full w-full p-0 data-[mobilenavbar=false]:hidden"
+          >
+            <MobileNavBar />
+          </div>
         </div>
+
         <div className="hidden w-full justify-items-center md:block md:w-full md:max-w-7xl">
           <ul className="flex w-full flex-col justify-between border p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
             <li className="content-center">
