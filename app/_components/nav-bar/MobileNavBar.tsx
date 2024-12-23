85cf1c39d57279f10df93607f85bdfc6a9ec06da
Barbara Sasfy <barbara.sasfy@gmail.com>
2024-12-23 11:46:49 +0000

    about page spacing

--- app/_components/nav-bar/MobileNavBar.tsx
+++ app/_components/nav-bar/MobileNavBar.tsx
@@ -1,10 +1,16 @@
-import Link from "next/link";
-
 export function MobileNavBar() {
   return (
     <div className="fixed left-0 top-[70px] h-[87vh] w-full bg-[#181818] sm:top-[90px]">
       <div className="w-full justify-items-center md:block md:w-full md:max-w-7xl">
         <ul className="flex w-full flex-col justify-between font-medium md:mt-0 md:flex-row md:space-x-8">
+          <li className="content-center">
+            <a
+              href="/"
+              className="block px-3 py-2 text-xl font-thin text-[#FFFBF2] hover:text-[#FFFBF2] md:border-0 md:hover:bg-transparent md:hover:text-blue-500 lg:text-3xl"
+            >
+              Home
+            </a>
+          </li>
           <li className="content-center">
             <a
               href="/about"
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,21 +3,23 @@ export default function AboutPage() {
     <main>
       <div className="bg-black">
         <div className="flex flex-col gap-5 px-10 py-20 text-[#FFFBF2] xl:px-24">
-          <div className="justify-self-center pb-5 text-4xl xl:font-light">
+          <div className="justify-self-center pb-9 text-3xl font-bold xl:text-4xl">
             About Us
           </div>
-          <div className="text-justify text-2xl xl:text-3xl">
+          <div className="text-justify text-xl xl:text-3xl">
             Our approach, whether its mental, physical or spiritual wellness you
-            are seeking, we spend time with our clients on their journey, Our
-            team in Kilmarnock have a combined experience of over 50 years
-            caring for and nurturing our clients when they come to us. As an
-            already established team of therapists at “ Time to Relax” in
+            are seeking, we spend time with our clients on their journey.
+            <br /> <br />
+            Our team in Kilmarnock have a combined experience of over 50 years
+            caring for and nurturing our clients when they come to us.
+            <br /> <br />
+            As an already established team of therapists at “Time to Relax” in
             Kilmarnock, we started Hidden Haven as a way continue to add to what
             we offer our clients, whether its laser treatments, massage and
             reflexology, facials and other holistic therapies to hypnotherapy
-            and counselling , we can offer you quality time for you.{" "}
+            and counselling, we can offer you quality time for you.
           </div>
-          <div className="text-justify text-2xl xl:text-3xl">
+          <div className="text-justify text-xl xl:text-3xl">
             We continue to grow our range of workshops and wellness days and
             treatments in line with client need and demand.
           </div>
@@ -25,7 +27,7 @@ export default function AboutPage() {
         </div>
       </div>
       <div className="relative h-[200px] w-full content-center justify-items-center bg-[url('/woman-behind-leaves.png')] bg-cover bg-center bg-no-repeat text-center">
-        <button className="left-[36vw] rounded-sm bg-[#EFD8B1] px-8 py-4 text-3xl">
+        <button className="left-[36vw] rounded-sm bg-[#C9AC7C] px-8 py-4 text-3xl">
           Book Now
         </button>
       </div>
