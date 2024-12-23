907f3c6668e7f1ef935a149996c972dfde3a6310
Barbara Sasfy <barbara.sasfy@gmail.com>
2024-12-23 11:22:26 +0000

    mobile menu positioning

--- app/_components/nav-bar/MobileNavBar.tsx
+++ app/_components/nav-bar/MobileNavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function MobileNavBar() {
   return (
-    <div className="fixed bottom-0 h-[87vh] w-full bg-[#181818]">
+    <div className="fixed left-0 top-[70px] h-[87vh] w-full bg-[#181818] sm:top-[90px]">
       <div className="w-full justify-items-center md:block md:w-full md:max-w-7xl">
         <ul className="flex w-full flex-col justify-between font-medium md:mt-0 md:flex-row md:space-x-8">
           <li className="content-center">
