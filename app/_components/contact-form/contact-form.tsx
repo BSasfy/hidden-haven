6f505ccc584431795ea26194af803fd94b4c8233
Barbara Sasfy <barbara.sasfy@gmail.com>
2025-01-19 22:44:45 +0000

    configure email form

--- app/_components/contact-form/contact-form.tsx
+++ app/_components/contact-form/contact-form.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
 
 export function ContactForm() {
   const form = useRef<HTMLFormElement>(null);
+  const [isSendButtonDisabled, setIsSendButtonDisabled] = useState(false);
+  const sendButtonLabel = isSendButtonDisabled ? "Sending..." : "Send";
+  function notifySent() {
+    toast("Email sent, we will be in touch soon!");
+  }
+  function notifyError() {
+    toast("Your email couldn't be sent. Please call us on 07392975905.");
+  }
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -26,49 +35,70 @@ export function ContactForm() {
       )
       .then(
         (result) => {
-          alert(result);
+          notifySent();
         },
         (error) => {
-          alert(error);
+          notifyError();
         },
       );
+
+    setIsSendButtonDisabled(true);
+
+    setTimeout(() => {
+      setIsSendButtonDisabled(false);
+      form.current?.reset();
+    }, 2000);
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-3">
-      <input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Name"
-        className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
-      />
-      <input
-        type="text"
-        id="number"
-        name="number"
-        placeholder="Contact Number"
-        className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
-      />
-      <input
-        type="text"
-        id="email"
-        name="email"
-        placeholder="Email"
-        className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
-      />
-      <input
-        type="text"
-        id="message"
-        name="message"
-        placeholder="Message..."
-        className="h-40 bg-[#FFFBF2] pl-2 text-gray-800"
-      />
-      <input
-        type="submit"
-        value="Send"
-        className="h-12 w-28 self-end bg-[#C9AC7C] text-xl md:border-[0.25px] md:border-[#FFFBF2] md:bg-[#181818]"
-      />
-    </form>
+    <div>
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-3">
+        <input
+          type="text"
+          id="from_name"
+          name="from_name"
+          placeholder="Name"
+          className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
+        />
+        <input
+          type="number"
+          id="phone_number"
+          name="phone_number"
+          placeholder="Contact Number"
+          className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
+        />
+        <input
+          type="email"
+          id="reply_to"
+          name="reply_to"
+          placeholder="Email"
+          className="h-8 bg-[#FFFBF2] pl-2 text-gray-800"
+          required
+        />
+        <textarea
+          name="text"
+          id="message"
+          placeholder="Message..."
+          rows={14}
+          cols={10}
+          wrap="soft"
+          className="h-40 bg-[#FFFBF2] px-2 text-gray-800"
+        />
+        {/* <input
+          type="textArea"
+          id="message"
+          name="message"
+          placeholder="Message..."
+          className="h-40 bg-[#FFFBF2] pl-2 text-gray-800"
+        /> */}
+        <input
+          type="submit"
+          value={sendButtonLabel}
+          disabled={isSendButtonDisabled}
+          className="h-12 w-28 self-end bg-[#C9AC7C] text-xl disabled:bg-gray-600 md:border-[0.25px] md:border-[#FFFBF2] md:bg-[#181818]"
+        />
+      </form>
+      <ToastContainer autoClose={2000} hideProgressBar={true} />
+    </div>
   );
 }
