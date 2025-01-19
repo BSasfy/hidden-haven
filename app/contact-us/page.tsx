3ea61a698aef1cc55e4dfe6173a66aca498bf9b6
Barbara Sasfy <barbara.sasfy@gmail.com>
2025-01-19 23:06:05 +0000

    change desktop hours to table

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -82,25 +82,36 @@ export default function ContactPage() {
             loading="lazy"
           ></iframe>
           <div className="content-center justify-items-center max-md:py-10 max-md:text-center md:bg-[#C9AC7C]">
-            <div className="pb-4 text-4xl max-md:font-thin max-md:italic">
+            <div className="pb-4 text-4xl max-md:font-thin max-md:italic md:text-5xl">
               Hours
             </div>
             <div className="text-3xl italic max-md:hidden">
-              <div className="flex">
-                <div className="pr-2 md:font-bold">Tuesday</div> 10:00 - 13:00
-              </div>
-              <div className="flex">
-                <div className="pr-2 md:font-bold">Wednesday</div> 09:00 - 19:00
-              </div>
-              <div className="flex">
-                <div className="pr-2 md:font-bold">Thursday</div> 09:00 - 19:00
-              </div>
-              <div className="flex">
-                <div className="pr-2 md:font-bold">Friday</div> 09:00 - 18:00
-              </div>
-              <div className="flex">
-                <div className="pr-2 md:font-bold">Saturday</div> 09:00 - 15:00
-              </div>
+              <table className="w-max">
+                <tr>
+                  <td className="font-bold">Monday </td>
+                  <td>Closed</td>
+                </tr>
+                <tr>
+                  <td className="font-bold">Tuesday</td>
+                  <td>10:00 - 13:00</td>
+                </tr>
+                <tr>
+                  <td className="pr-8 font-bold">Wednesday</td>
+                  <td>09:00 - 19:00</td>
+                </tr>
+                <tr>
+                  <td className="font-bold">Thursday</td>
+                  <td>09:00 - 19:00</td>
+                </tr>
+                <tr>
+                  <td className="font-bold">Friday</td>
+                  <td>09:00 - 18:00</td>
+                </tr>
+                <tr>
+                  <td className="font-bold">Saturday</td>
+                  <td>09:00 - 15:00</td>
+                </tr>
+              </table>
             </div>
             <div className="text-center text-2xl font-thin italic md:hidden">
               <div>Tuesday 10:00 - 13:00</div>
