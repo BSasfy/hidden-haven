23b442e0c2e3a309ed0dd7de077a066b27f71f96
Barbara Sasfy <barbara.sasfy@gmail.com>
2025-01-22 23:10:50 +0000

    front page button and title fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function HomePage() {
           alt="hands holding flower"
           className="object-cover lg:object-contain"
         />
-        <div className="mx-auto mb-[250px] block w-4/5 justify-self-center text-center text-3xl font-medium italic text-white md:w-1/2 md:text-4xl lg:text-5xl">
+        <div className="relative mx-auto mb-[250px] block w-4/5 justify-self-center text-center text-3xl font-medium italic text-white md:w-1/2 md:text-4xl lg:text-5xl">
           For mental, physical and spiritual wellbeing
         </div>
 
         <Link
-          className="mx-auto mb-9 block rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl"
+          className="relative mx-auto mb-9 block rounded-sm bg-[#C9AC7C] px-4 py-2 text-3xl"
           href="https://the-haven-shop.square.site/s/appointments"
         >
           Book Now
