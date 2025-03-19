7ef94f1a2eeb09d452f1915487a9b3696ce4ed37
BSasfy <barbara.sasfy@gmail.com>
2025-03-19 10:16:30 +0000

    remove alert from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
-import Alert from "@mui/material/Alert";
 
 export default function HomePage() {
   return (
     <main>
-      <Alert severity="info">
-        You can now book some of our services via the{" "}
-        <Link
-          href="https://the-haven-shop.square.site/s/appointments"
-          className="font-bold underline"
-        >
-          BOOK NOW
-        </Link>{" "}
-        button. If you can&apos;t see what you&apos;re looking for, please call
-        us on 07392975905! We&apos;re working on making all the services
-        available for booking online.
-      </Alert>
       <div className="relative flex h-[80vh] w-full flex-col content-center justify-end">
         <Image
           src="/homepage-hands.jpg"
