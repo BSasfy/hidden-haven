11a8b03e3330090337d7cc0923b49c2e284884cf
BSasfy <barbara.sasfy@gmail.com>
2025-03-19 15:38:59 +0000

    remove alert from services pages

--- app/_components/services-button/ServicesButton.tsx
+++ app/_components/services-button/ServicesButton.tsx
@@ -10,18 +10,6 @@ export function ServicesButton() {
       >
         Book Now
       </Link>
-      <Alert severity="info" className="mt-6">
-        You can now book some of our services via the{" "}
-        <Link
-          href="https://the-haven-shop.square.site/s/appointments"
-          className="font-bold underline"
-        >
-          BOOK NOW
-        </Link>
-        &nbsp;button. If you can&apos;t see what you&apos;re looking for, please
-        call us on 07392975905! We&apos;re working on making all the services
-        available for booking online.
-      </Alert>
     </div>
   );
 }
